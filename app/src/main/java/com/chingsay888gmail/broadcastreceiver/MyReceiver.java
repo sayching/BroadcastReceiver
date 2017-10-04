dafe20ca9b4081b44339d7631c64745da17868c5
CypherGin <chingsay888@gmail.com>
2017-10-04 08:24:25 +0700

    Initial commit

---
+++ app/src/main/java/com/chingsay888gmail/broadcastreceiver/MyReceiver.java
@@ -0,0 +1,22 @@
+package com.chingsay888gmail.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by ching on 10/4/2017.
+ */
+
+public class MyReceiver extends BroadcastReceiver
+{
+    public MyReceiver()
+    {
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
